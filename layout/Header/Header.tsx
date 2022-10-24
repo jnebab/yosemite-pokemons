64cd05ff570102cb572a002c01be7da5a62983d7
John Carl Nebab <nebab.johncarl@gmail.com>
2022-10-24 16:26:03 +0800

    fix: layout issues in header for mobile screen

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,27 +1,78 @@
-import { Flex } from "@chakra-ui/react";
-import ThemeToggle from "../ThemeToggle";
-import { Heading } from "@chakra-ui/react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import NextLink from "next/link";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Show,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import ThemeToggle from "../ThemeToggle";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
+  const menuRef = useRef<RefObject<any>>();
+
+  useEffect(() => {
+    if (isOpen) {
+      onClose();
+    }
+  }, [router.asPath]);
+
   return (
     <Flex
       as="header"
       width="full"
       align="center"
       justify="space-between"
-      p={10}
+      p={{
+        base: 6,
+        md: 10,
+      }}
       h={"80px"}
       mb={6}
     >
       <Heading>Pokemites</Heading>
-      <Flex gap={6}>
-        <Flex gap={4} align="center">
-          <NextLink href="/">Home</NextLink>
-          <NextLink href="/my-pokemons">My Pokemons</NextLink>
+      <Show above="md">
+        <Flex gap={6}>
+          <Flex gap={4} align="center">
+            <NextLink href="/">Home</NextLink>
+            <NextLink href="/my-pokemons">My Pokemons</NextLink>
+          </Flex>
+          <ThemeToggle />
         </Flex>
-        <ThemeToggle />
-      </Flex>
+      </Show>
+      <Show below="md">
+        <Flex gap={6} align="center">
+          <ThemeToggle />
+          <GiHamburgerMenu onClick={onOpen} />
+        </Flex>
+      </Show>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={menuRef as RefObject<any>}
+        isFullHeight
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody display="grid" height="100%" placeItems="center">
+            <Flex gap={4} align="center" direction="column">
+              <NextLink href="/">Home</NextLink>
+              <NextLink href="/my-pokemons">My Pokemons</NextLink>
+            </Flex>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Flex>
   );
 }
